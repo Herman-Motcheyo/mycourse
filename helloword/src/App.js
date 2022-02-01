7b9e49b63edff7c309ec0b4a813203535bed7649
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2022-02-01 17:20:11 +0100

    day 1

---
+++ helloword/src/App.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+import './App.css';
+
+function App(){
+  return(
+    <Fragment>
+       <p> Helloword Herman  🙂 I make this challenge </p>
+      </Fragment>
+  )
+}
+export default App ;
